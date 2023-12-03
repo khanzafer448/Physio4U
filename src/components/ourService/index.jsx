b2997ed75d54daad0271ac571f19f18ef5fe21ab
websul123 <syedwebsul123@gmail.com>
2023-12-03 23:09:43 +0530

    form updates

--- src/components/ourService/index.jsx
+++ src/components/ourService/index.jsx
@@ -1,133 +1,57 @@
 // import React from 'react'
 // import "swiper/swiper-bundle.min.css";
+import PropTypes from "prop-types";
+
+const data = [
+  {
+    title: "Chronic back Pain",
+    img: "/img/whyUs/muscle-pain.png",
+  },
+  {
+    title: "Neck Pain",
+    img: "/img/whyUs/neckPain.png",
+  },
+  {
+    title: "Post-accident Physiotherapy",
+    img: "/img/whyUs/rehabilitation.png",
+  },
+  {
+    title: "Frozen shoulder",
+    img: "/img/whyUs/chiropractic.png",
+  },
+  {
+    title: "Paraplegia",
+    img: "/img/whyUs/massage.png",
+  },
+  {
+    title: "Quadriplegia",
+    img: "/img/whyUs/back.png",
+  },
+  {
+    title: "Back pain by Slipped Disc",
+    img: "/img/whyUs/backPain.png",
+  },
+  {
+    title: "Paralysis",
+    img: "/img/whyUs/exercise-ball.png",
+  },
+];
 
 const OurService = () => {
   return (
     <div className="service-wrap ptb-100" id="services">
-      <div className="container-fluid">
-        <div className="row align-items-center">
-          <div
-            className="col-xxl-3 col-xl-4 col-lg-4"
-            data-aos="fade-right"
-            data-aos-duration="1200"
-            data-aos-delay="200"
-          >
-            <div className="service-content">
-              <div className="content-title-one">
-                <span>Our Services</span>
-                <h2>We Offer Various Physio Disciplines</h2>
-                <p>
-                  We take pride in offering a diverse range of physiotherapy
-                  disciplines, providing tailored solutions to meet the unique
-                  needs of our patients.
-                </p>
-                <a href="#" className="btn-one">
-                  View All Servises
-                </a>
-              </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-6 offset-xl-3 col-lg-8 offset-lg-2">
+            <div className="section-title-one text-center mb-30">
+              <span>Our Services</span>
+              <h2>We Offer Various Physio Disciplines</h2>
             </div>
           </div>
-          <div className="col-xxl-9 col-xl-8 col-lg-8">
-            <div className="swiper service-slider-one">
-              <div className="swiper-wrapper">
-                <div className="swiper-slide">
-                  <div className="service-card-one">
-                    <div className="service-img">
-                      <img src="img/services/service-1.webp" alt="Image" />
-                    </div>
-                    <div className="service-info">
-                      <h3>
-                        <a href="#">Physiotherapy Treatment</a>
-                      </h3>
-                      <p>
-                        It is a long established fact that eade will be
-                        distracted by the readable
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="swiper-slide">
-                  <div className="service-card-one">
-                    <div className="service-img">
-                      <img src="img/services/service-2.webp" alt="Image" />
-                    </div>
-                    <div className="service-info">
-                      <h3>
-                        <a href="#">Spinal Decompression</a>
-                      </h3>
-                      <p>
-                        It is a long established fact that eade will be
-                        distracted by the readable
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="swiper-slide">
-                  <div className="service-card-one">
-                    <div className="service-img">
-                      <img src="img/services/service-3.webp" alt="Image" />
-                    </div>
-                    <div className="service-info">
-                      <h3>
-                        <a href="#">Acupuncture Treatment</a>
-                      </h3>
-                      <p>
-                        It is a long established fact that eade will be
-                        distracted by the readable
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="swiper-slide">
-                  <div className="service-card-one">
-                    <div className="service-img">
-                      <img src="img/services/service-4.webp" alt="Image" />
-                    </div>
-                    <div className="service-info">
-                      <h3>
-                        <a href="#">Fitness Treatment</a>
-                      </h3>
-                      <p>
-                        It is a long established fact that eade will be
-                        distracted by the readable
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="swiper-slide">
-                  <div className="service-card-one">
-                    <div className="service-img">
-                      <img src="img/services/service-5.webp" alt="Image" />
-                    </div>
-                    <div className="service-info">
-                      <h3>
-                        <a href="#">Personalized Physio Treatment</a>
-                      </h3>
-                      <p>
-                        It is a long established fact that eade will be
-                        distracted by the readable
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="swiper-slide">
-                  <div className="service-card-one">
-                    <div className="service-img">
-                      <img src="img/services/service-6.webp" alt="Image" />
-                    </div>
-                    <div className="service-info">
-                      <h3>
-                        <a href="#">Neurological Therapy</a>
-                      </h3>
-                      <p>
-                        It is a long established fact that eade will be
-                        distracted by the readable
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div className="why-us-wrapper">
+            {data.map((item, i) => (
+              <IndividualCard key={i} item={item} />
+            ))}
           </div>
         </div>
       </div>
@@ -136,3 +60,28 @@ const OurService = () => {
 };
 
 export default OurService;
+
+const IndividualCard = ({ item }) => {
+  return (
+    <div
+      className="why-us-content wh-card-one"
+      data-aos="fade-up"
+      data-aos-duration="1200"
+      data-aos-delay="300"
+    >
+      <img src={item?.img} alt={item?.title} />
+      <p>{item?.title}</p>
+    </div>
+  );
+};
+
+IndividualCard.defaultProps = {
+  item: {
+    title: "added soon",
+    img: "/img/logo.png",
+  },
+};
+
+IndividualCard.propTypes = {
+  item: PropTypes.object,
+};
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,7 @@ import IntroVideo from "../components/introVideo";
 import Layout from "../components/layout/layout";
 import MoveText from "../components/moveText";
 import OurService from "../components/ourService";
-import OurTeam from "../components/ourTeam";
+// import OurTeam from "../components/ourTeam";
 import WhyUS from "../components/whyUs";
 import AppointmentForm from "../components/appointmentForm";
 
@@ -13,14 +13,14 @@ const Home = () => {
   return (
     <Layout>
       <HeroBanner />
+      <AppointmentForm />
       <Feature />
       <About />
       <WhyUS />
-      <AppointmentForm />
       <MoveText />
       <OurService />
       <IntroVideo />
-      <OurTeam /> 
+      {/* <OurTeam /> */}
     </Layout>
   );
 };
